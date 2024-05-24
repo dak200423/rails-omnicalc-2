a67c4899c95a3feefc103b8536445fb0f8c9895e
dak200423 <166195026+dak200423@users.noreply.github.com>
2024-05-24 12:03:00 +0000

    project complete

--- app/controllers/division_controller.rb
+++ app/controllers/division_controller.rb
@@ -4,11 +4,10 @@ class DivisionController < ApplicationController
   end
 
   def divide_these
-    @first_number = params.fetch("first_num")
-    @second_number = params.fetch("second_num").to_f
+    @first_num = params.fetch("first_num_div").to_f
+    @second_num = params.fetch("second_num_div").to_f
+    @result_div = @first_num / @second_num
 
-    @result =  first_number / @second_number
-
-    render({ :template => "division_templates/divide_result" })
+    render({ :template => "division_templates/divide_results" })
   end
 end
